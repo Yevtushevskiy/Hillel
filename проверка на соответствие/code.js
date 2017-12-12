770723c705b4ffc76dc69b3be802e47267f210fe
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2017-12-12 15:39:42 +0200

    добавил езё одно задание

--- проверка на соответствие/code.js
+++ проверка на соответствие/code.js
@@ -7,3 +7,27 @@ function equal2(t,r){
 
 	return t === r ;
 }
+
+
+function sr2(a,b){
+	return (a+b)/2;
+
+}
+
+function srManyNum(a){
+	var arr= a.split (',');
+	var sum=0;
+	for (var i=0; i<arr.length; i++){
+		sum += Number(arr[i]);
+	}
+	return sum/arr.length;
+}
+
+	function testWork(age,exp){
+	age=Number(age);
+	if(age<30 && age>20 && exp==true){
+		return true;
+
+	}
+	else{return(false) }
+}
